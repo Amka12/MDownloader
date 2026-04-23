33dd47511612c29e0c28804a935979dad8d3247f
Amka12 <80223403+Amka12@users.noreply.github.com>
2026-04-23 18:51:12 +0300

    Кастомный titlebar с кнопками

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
-using MDownloader.ViewModels;
+﻿using MDownloader.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace MDownloader;
 
@@ -9,5 +11,50 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         DataContext = viewModel;
+
+        StateChanged += (s, e) => UpdateMaximizeIcon();
+        UpdateMaximizeIcon();
+    }
+
+    private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ClickCount == 2)
+        {
+            ToggleMaximize();
+        }
+        else
+        {
+            DragMove();
+        }
+    }
+
+    private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void BtnMaximize_Click(object sender, RoutedEventArgs e)
+    {
+        ToggleMaximize();
+    }
+
+    private void ToggleMaximize()
+    {
+        WindowState = WindowState == WindowState.Maximized
+            ? WindowState.Normal
+            : WindowState.Maximized;
+    }
+
+    private void BtnClose_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void UpdateMaximizeIcon()
+    {
+        if (BtnMaximize?.Content is TextBlock textBlock)
+        {
+            textBlock.Text = WindowState == WindowState.Maximized ? "🗗" : "❐";
+        }
     }
 }
